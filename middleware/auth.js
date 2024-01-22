a6582004ee2da80a0222114d88a4aa6785703129
Tharun2511 <106338057+Tharun2511@users.noreply.github.com>
2024-01-23 00:27:45 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
+  
   if (!config.get("requiresAuth")) return next();
 
   const token = req.header("x-auth-token");
@@ -11,7 +12,8 @@ module.exports = function(req, res, next) {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
-  } catch (ex) {
+  } 
+  catch (ex) {
     res.status(400).send("Invalid token.");
   }
 };
